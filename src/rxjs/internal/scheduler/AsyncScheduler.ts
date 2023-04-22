571d23fb36da51fe3f6f57188b429c0258a6a998
greenmashimaro@gmail.com <greenmashimaro@gmail.com>
2023-04-22 22:28:14 +0800

    feat: AsyncScheduler

--- src/rxjs/internal/scheduler/AsyncScheduler.ts
+++ src/rxjs/internal/scheduler/AsyncScheduler.ts
@@ -3,11 +3,39 @@ import { Action } from './Action'
 import { AsyncAction } from './AsyncAction'
 
 export class AsyncScheduler extends Scheduler {
+  public _active: boolean = false
+  public actions: Array<AsyncAction<any>> = []
+
   constructor(SchedulerAction: typeof Action, now: () => number = Scheduler.now) {
     super(SchedulerAction, now)
   }
 
   public flush(action: AsyncAction<any>): void {
-    action.execute(action.state, action.delay)
+    const actions = this.actions
+    if (this._active) {
+      this.actions.push(action)
+      return
+    }
+
+    let error: any
+
+    this._active = true
+    do {
+      error = action.execute(action.state, action.delay)
+      if (error) {
+        break
+      }
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    } while ((action = actions.shift()!))
+    this._active = false
+
+    if (error) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      while ((action = actions.shift()!)) {
+        action.unsubscribe()
+      }
+
+      throw error
+    }
   }
 }
